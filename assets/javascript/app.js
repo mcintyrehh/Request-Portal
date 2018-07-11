d9d9f63338b516535c7036b7a83c0dc416a9f197
Onray <mcintyrehh@gmail.com>
2018-07-11 19:07:22 -0400

    on hover cards now display movie title, year, imdbID, and an add button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,13 +6,14 @@ var queryURL;
 
 
 $("#search-by-title-button").on('click', function() {
+    $('body').find('.posters').empty();
     title = $("#t").val();
     year = $("#y").val();
     if (year && title) {
         queryURL = "https://www.omdbapi.com/?s=" + title + "&y=" + year + "&type=movie&apikey=e73085d6";
     }
     else if (!year) {
-        queryURL="https://www.omdbapi.com/?s=" + title + "&y=&type=movie&apikey=e73085d6";
+        queryURL = "https://www.omdbapi.com/?s=" + title + "&y=&type=movie&apikey=e73085d6";
     }
     else {
 
@@ -33,12 +34,14 @@ $("#search-by-title-button").on('click', function() {
         $(".posters").append('<div class="card m-2 bg-dark text-center text-white">'+
         '<img class="card-img" src="'+ cardPoster + '" alt="test poster">'+
         '<div class="card-img-overlay d-flex flex-column justify-content-end hover-text">'+
-            '<h5 class="card-title movie-name">'+ array.Search[x].Title + '</h5>'+
-            '<p class="card-text movie-year">' + array.Search[x].Title + '</p>'+
-        '</div>'+
+            '<h5 class="card-title movie-name hide">'+ array.Search[x].Title + '</h5>'+
+            '<p class="card-text movie-year hide">' + array.Search[x].Year + '</p>'+
+            '<p class="card-text col-small movie-IMDb hide"> IMDb id: ' + array.Search[x].imdbID + '</p>' +
+            '<button  class="btn btn-success hide">Add Movie</button>' +
+            '</div>'+
     '</div>');
-        $(".movie-name").hide(); 
-        $(".movie-year").hide();
+        $(".hide").hide(); 
+        
     }   
     for (var i = 0; i<10; i++){   
         newCard(i);
@@ -46,26 +49,26 @@ $("#search-by-title-button").on('click', function() {
     $(".card" ).hover(
         function() {
           $(this).find(".card-img").css("opacity", "0.2");
-          $(this).find(".movie-name").show();
-          $(this).find(".movie-year").show();
+          $(this).find(".hide").show();
+        //   $(this).find(".movie-year").show();
         }, function() {
           $(this).find(".card-img").css("opacity", "1");
-          $(this).find(".movie-name").hide();
-          $(this).find(".movie-year").hide();
+          $(this).find(".hide").hide();
+        //   $(this).find(".movie-year").hide();
         }
       );
     });
    
 });
 
-$(".card" ).hover(
-    function() {
-      $(this).find(".card-img").css("opacity", "0.2");
-      $(this).find(".movie-name").show();
-      $(this).find(".movie-year").show();
-    }, function() {
-      $(this).find(".card-img").css("opacity", "1");
-      $(this).find(".movie-name").hide();
-      $(this).find(".movie-year").hide();
-    }
-  );
+// $(".card" ).hover(
+//     function() {
+//       $(this).find(".card-img").css("opacity", "0.2");
+//       $(this).find(".movie-name").show();
+//       $(this).find(".movie-year").show();
+//     }, function() {
+//       $(this).find(".card-img").css("opacity", "1");
+//       $(this).find(".movie-name").hide();
+//       $(this).find(".movie-year").hide();
+//     }
+//   );
