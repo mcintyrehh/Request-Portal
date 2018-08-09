49fb6759cd5bb826dfae229f7bfd5dc3a6cfdceb
Onray <mcintyrehh@gmail.com>
2018-08-09 18:53:00 -0400

    added intro gif

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,8 +4,17 @@ var title;
 var year;
 var queryURL;
 
+var hoverFace = $(".my-face");
+    
+hoverFace.slideDown(3000);
 
-$("#search-by-title-button").on('click', function() {
+function removeFace() {
+   hoverFace.slideUp(3000);
+   }
+// hoverFace.attr("background-color", "#111");
+window.setTimeout(removeFace, 4000);
+
+$("#search-by-title-button").on('click', function () {
     $('body').find('.posters').empty();
     title = $("#t").val();
     year = $("#y").val();
@@ -20,55 +29,61 @@ $("#search-by-title-button").on('click', function() {
     }
     console.log(queryURL);
     $.ajax({
-    url: queryURL,
-    method: "GET"
-    }).then(function(response) {
-    var array = response;
-    console.log(response);
-    console.log(array.Search[0].Title);
-    
-    function newCard(x) {
-        // var cardTitle = array.Search[x].Title;
-        // var cardYear = array.Search[x].Year;
-        var cardPoster = array.Search[x].Poster;
-        $(".posters").append('<div class="card m-2 bg-dark text-center text-white">'+
-        '<img class="card-img" src="'+ cardPoster + '" alt="test poster">'+
-        '<div class="card-img-overlay d-flex flex-column justify-content-end hover-text">'+
-            '<h5 class="card-title movie-name hide">'+ array.Search[x].Title + '</h5>'+
-            '<p class="card-text movie-year hide">' + array.Search[x].Year + '</p>'+
-            '<p class="card-text col-small movie-IMDb hide"> IMDb id: ' + array.Search[x].imdbID + '</p>' +
-            '<button  class="btn btn-success hide">Add Movie</button>' +
-            '</div>'+
-    '</div>');
-        $(".hide").hide(); 
-        
-    }   
-    for (var i = 0; i<10; i++){   
-        newCard(i);
-    }
-    $(".card" ).hover(
-        function() {
-          $(this).find(".card-img").css("opacity", "0.2");
-          $(this).find(".hide").show();
-        //   $(this).find(".movie-year").show();
-        }, function() {
-          $(this).find(".card-img").css("opacity", "1");
-          $(this).find(".hide").hide();
-        //   $(this).find(".movie-year").hide();
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var array = response;
+        console.log(response);
+        console.log(array.Search[0].Title);
+
+        function newCard(x) {
+            // var cardTitle = array.Search[x].Title;
+            // var cardYear = array.Search[x].Year;
+            var cardPoster = array.Search[x].Poster;
+            $(".posters").append('<div class="card m-2 bg-dark text-center text-white">' +
+                '<img class="card-img" src="' + cardPoster + '" alt="test poster">' +
+                '<div class="card-img-overlay d-flex flex-column justify-content-end hover-text">' +
+                '<h5 class="card-title movie-name hide">' + array.Search[x].Title + '</h5>' +
+                '<p class="card-text movie-year hide">' + array.Search[x].Year + '</p>' +
+                '<p class="card-text col-small movie-IMDb hide"> IMDb id: ' + array.Search[x].imdbID + '</p>' +
+                '<button  class="btn btn-success hide add-movie" data-title="' + array.Search[x].Title + '" data-id="' + array.Search[x].imdbID + '" data-url="' + array.Search[x].Poster + '">Add Movie</button>' +
+                '</div>' +
+                '</div>');
+            $(".hide").hide();
+
         }
-      );
+        for (var i = 0; i < 10; i++) {
+            newCard(i);
+        }
+        $(".card").hover(
+            function () {
+                $(this).find(".card-img").css("opacity", "0.2");
+                $(this).find(".hide").show();
+            }, function () {
+                $(this).find(".card-img").css("opacity", "1");
+                $(this).find(".hide").hide();
+                //   $(this).find(".movie-year").hide();
+            }
+        );
     });
-   
+    // $("body").on("click", ".add-movie", function () {
+    //     console.log("gotcha!");
+    //     var newObj = {
+    //         title: $(this).data("title"),
+    //         imdb_id: $(this).data("id"),
+    //         poster_url: $(this).data("url")
+    //     }
+    //     var stringNewObj = JSON.stringify(newObj);
+    //     console.log(stringNewObj);
+    //     fs.appendFile(fileJSON, stringNewObj, function (err) {
+    //         if (err) {
+    //             console.log(err);
+    //         }
+    //         else {
+    //             console.log("Movie Added!")
+    //         }
+    //     })
+    // })
 });
 
-// $(".card" ).hover(
-//     function() {
-//       $(this).find(".card-img").css("opacity", "0.2");
-//       $(this).find(".movie-name").show();
-//       $(this).find(".movie-year").show();
-//     }, function() {
-//       $(this).find(".card-img").css("opacity", "1");
-//       $(this).find(".movie-name").hide();
-//       $(this).find(".movie-year").hide();
-//     }
-//   );
+
